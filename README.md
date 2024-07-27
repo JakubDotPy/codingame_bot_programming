4ca6a5ec71bd5502e2e07d37c52fca4a824226a0
Jakub Cervinka <cervinka.jakub.1989@gmail.com>
2024-07-27 19:52:58 +0200

    new code for coders_strike_back
    updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# codingame_bot_programming
-bots for codingame bot programming challenges
+# Codingame bots
+
+Backup of bots for codingame [bot programming challenges](https://www.codingame.com/multiplayer/bot-programming).
+
+_NOTE: Code in this repository is only a backup, it is not intended to be run directly._
+
+---
+
+### MAD POD RACING aka Coders strike back
+
+[[homepage](https://www.codingame.com/multiplayer/bot-programming/mad-pod-racing)]  [[code](coders_strike_back)]<br>
+This puzzle game starts with a step by step tutorial that will help you get familiar with CodinGame’s multiplayer games.
+It provides an easy introduction to bot programming through a starship race.
+
+The aim of the game is of course to win the race against other players! To succeed in this challenge, you will be able
+to use different mathematical concepts such as trajectory calculation, collisions, speed vector, or inertia.
+
+The game is very simple to start. Rules are easy to understand and it only requires a few lines of code to move your
+ship around.
+
+However, it has near-infinite possibilities of evolution as you can improve your artificial intelligence step by step,
+while sharpening your coding skills.
+
+---
+
+### Olymbits
+
+[[homepage](https://www.codingame.com/multiplayer/bot-programming/summer-challenge-2024-olymbits)]  [[code](olymbits)]<br>
+Three players are pitted against one another in the arcade olympics.
+
+Each player controls a character in four mini-games simultaneously. Earn a maximum of medals in all four games to
+acquire the highest score.
+
+The game is played in turns. On each turn, all three players perform one of four possible actions: UP, DOWN, LEFT, or
+RIGHT.
+
+When an action is performed by a player, their agent in each mini-game performs that same action, because the controls
+have been wired to all 4 machines at once.
+
+---
+
+### Seabed security
+
+[[homepage](https://www.codingame.com/multiplayer/bot-programming/seabed-security)]  [[code](seabed_security)]<br>
+To protect marine life, it is crucial to understand it. Explore the ocean floor using your drones to scan as many fish
+as possible to better understand them!
+
+Win more points than your opponent by scanning the most fish.
--- coders_strike_back/bronze.py
+++ coders_strike_back/bronze.py
@@ -1,166 +1,250 @@
-import sys
+import collections
 import math
+import sys
+from dataclasses import dataclass
+from dataclasses import field
+from functools import cached_property
+from functools import partial
+
 import numpy as np
 
-# print(f'{first_round=}', file=sys.stderr, flush=True)
+log = partial(print, file=sys.stderr, flush=True)
 
-checkpoints = []
 
-first_round = True
-boost_used = False
-BOOST_DIST = 5000
-BOOST_ANGLE = 3
+class Params:
+    SLOW_ANGLE = 90
 
-PRESTEER_DIST = 1500
-PRESTEER_ANGLE = 20
+    SLOWDOWN_DIST = 1000  # how far should the slowdown process start
+    SLOWDOWN_DIST_FACTOR = 20
 
-SLOWDOWN_DIST = 2000  # how far should the slowdown process start
-SLOWDOWN_DIST_FACTOR = 40
-SLOW_ANGLE = 120
+    BOOST_DIST = 2000
+    BOOST_ANGLE = 3
 
-def _angle_speed(angle):
-    if abs(angle) > SLOW_ANGLE:
-        return 0
-    else:
-        return max(0, 100 - abs(angle))
 
-def _dist_speed(distance):
-    """strategies
-    a) linear
-        return 100 if distance > SLOWDOWN_DIST else min(100, distance // SLOWDOWN_DIST_FACTOR)
-    b) modified_exp
-        break_fn = lambda (dist, fac, slow): min(100, math.pow(fac, dist/(slow/2)))) 
-        return break_fn()
-    c) 100 or 0 based on distance
-        return 0 if dist < SLOWDOWN_DIST else 100 
-    
-    """
-    strategies = {
-        'a' : lambda d, f, s_d: 100 if d > s_d else min(100, d // f),
-        'b' : lambda d, f, s_d: min(100, math.pow(f, d/(s_d/2))),
-        'c' : lambda d, f, s_d: min(100, math.pow(f, int(d/(s_d/2))+20)), 
-    }
-    # pick strategy
-    break_fn = strategies['a']
-    return break_fn(distance, SLOWDOWN_DIST_FACTOR, SLOWDOWN_DIST)
-    
-def _get_boost(target):
-    global boost_used
+# ========== helper functions ==========
 
-    if first_round and not boost_used:
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
 
-        if target['dist'] > BOOST_DIST and abs(target['angle']) < BOOST_ANGLE:
-            boost_used = True
+
+def angle_between(v1, v2):
+    # TODO: we dont know our bearing.. cant calculate proper angle
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.rad2deg(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+
+
+# ========== base classes ==========
+
+class State:
+    def action(self, context):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def next_state(self, context):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+@dataclass(kw_only=True)
+class Point:
+    pos: np.ndarray = field(default_factory=lambda: np.array([0, 0]))
+
+    def interpolate_percent(self, dest: 'Point', percent):
+        return ((1 - percent) * self.pos + percent * dest.pos).astype(int)
+
+    def interpolate_units(self, dest: 'Point', amt: int):
+        direction = dest.pos - self.pos
+        direction_normalized = direction / np.linalg.norm(direction)
+        X = self.pos + direction_normalized * amt
+        return X.astype(int)
+
+
+# ========== reusable strategies ==========
+
+def steer_strategy(player: 'Player'):
+    """determine future target"""
+    # steering adjust
+    p = 1.2
+    target = player.interpolate(player.next_cp, p)
+    return target
+
+
+def steer_strategy(player: 'Player'):
+    """determine future target"""
+    return player.next_cp.aimpoint
+
+
+def thrust_strategy(player: 'Player'):
+    """determine future thrust"""
+
+    def by_angle(angle):
+        if abs(angle) > Params.SLOW_ANGLE:
+            return 30
+        else:
+            return 100
+
+    def by_distance(distance):
+        value = math.pow(Params.SLOWDOWN_DIST_FACTOR, distance // (Params.SLOWDOWN_DIST // 2))
+        return min(100, value)
+
+    def boost():
+        if (
+                not player.boost_used
+                and player.next_cp.distance > Params.BOOST_DIST
+                and player.next_cp.angle < Params.BOOST_ANGLE
+
+        ):
+            player.boost_used = True
             return 'BOOST'
         else:
             return None
 
-def get_speed(target):
-    
-    thrust = min((
-        _angle_speed(next_checkpoint['angle']),
-        _dist_speed(next_checkpoint['dist'])
-    ))
-
-    boost = _get_boost(target)
+    thrust = min(
+        by_angle(player.next_cp.angle),
+        by_distance(player.next_cp.distance),
+    )
+    boost = boost()
 
     return boost or int(thrust)
 
-def get_target():
 
-    # dont presteer in the first round
-    if first_round: 
-        return next_checkpoint
-    
-    # conditions
-    is_close_to_next = next_checkpoint['dist'] < PRESTEER_DIST
-    is_alligned_to_next = next_checkpoint['angle'] < PRESTEER_ANGLE
+# ========== state ==========
 
-    # decision
-    can_turn = is_close_to_next and is_alligned_to_next
-    
-    # select target
-    target = future_checkpoint if can_turn else next_checkpoint
 
-    print(f'in target {target=}', file=sys.stderr, flush=True)
+class FirstRound(State):
+    def action(self, context):
+        steer = steer_strategy(context)
+        thrust = thrust_strategy(context)
+        print(*steer, thrust)
 
-    return target
+    def next_state(self, context):
+        # TODO: add proper transition after first round
+        return Race()
 
-def move():
 
-    target = get_target()
-    thrust = get_speed(target)
+class Race(State):
+    def action(self, context):
+        steer = steer_strategy(context)
+        thrust = thrust_strategy(context)
+        print(*steer, thrust)
 
-    return *target['pos'], thrust
+    def next_state(self, context):
+        return self
 
-def read_game_state():
-    global first_round
 
-    x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
-    opponent_x, opponent_y = [int(i) for i in input().split()]
+class StateMachine:
+    def __init__(self, initial_state):
+        self.context = None
+        self.state = initial_state
 
-    my_pos = np.array([x, y])
-    his_pos = np.array([opponent_x, opponent_y])
+    def set_context(self, context):
+        self.context = context
 
-    enemy_dist = np.linalg.norm(my_pos-his_pos)
+    def action(self):
+        if self.state is None:
+            raise SystemExit("State machine is empty")
 
-    next_checkpoint = np.array([next_checkpoint_x, next_checkpoint_y])
-    _next_checkpoint = (next_checkpoint_x, next_checkpoint_y)
+        self.state.action(self.context)
+        self.state = self.state.next_state(self.context)
 
-    future_checkpoint = None
-    future_dist = None
 
-    if _next_checkpoint not in checkpoints:
-        checkpoints.append(_next_checkpoint)
-    if len(checkpoints) > 1 and _next_checkpoint == checkpoints[0]:
-        first_round = False
-    if first_round == False:
-        index = checkpoints.index(_next_checkpoint)
-        future_checkpoint = np.array(checkpoints[(index + 1) % len(checkpoints)])
-        future_dist = np.linalg.norm(future_checkpoint-my_pos)
+# ========== game entities ==========
 
-    #print(f'{checkpoints=}', file=sys.stderr, flush=True)
-    #print(f'{next_checkpoint=}', file=sys.stderr, flush=True)
-    #print(f'{future_checkpoint=}', file=sys.stderr, flush=True)
+ARENA_CENTER = Point(pos=np.array([8000, 4500]))
 
-    def unit_vector(vector):
-        return vector / np.linalg.norm(vector)
 
-    def angle_between(v1, v2):
-        # TODO: we dont know our bearing.. cant calculate proper angle
-        v1_u = unit_vector(v1)
-        v2_u = unit_vector(v2)
-        return np.rad2deg(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
-    
-    try:
-        future_checkpoint_angle = angle_between(my_pos, future_checkpoint)
-        #print(f'{future_checkpoint_angle=}', file=sys.stderr, flush=True)
-    except:
-        # we dont have future vector
-        future_checkpoint_angle = None
+@dataclass
+class Checkpoint(Point):
+    angle: int = 0
+    distance: int = 0
+    radius: int = 600
 
-    game_state = {
-        'my_pos'          : my_pos,
-        'his_pos'         : his_pos,
-        'next_checkpoint' : {
-            'pos'  : next_checkpoint,
-            'angle': next_checkpoint_angle,
-            'dist' : next_checkpoint_dist,
-        },
-        'future_checkpoint': {
-            'pos'  : future_checkpoint,
-            'angle': future_checkpoint_angle,
-            'dist' : future_dist,
-        },
-        'first_round'     : first_round,
-        'enemy_dist'      : enemy_dist,
-    }
+    @cached_property
+    def aimpoint(self):
+        return self.interpolate_units(ARENA_CENTER, self.radius)
 
-    globals().update(game_state)
 
-    print(f'{first_round=}', file=sys.stderr, flush=True)
+@dataclass
+class Pod(Point):
+    pass
 
-# game loop
-while True:
-    read_game_state()
-    print(*move())
+
+@dataclass
+class Player(Pod):
+    initial_state: State
+    state_machine: StateMachine = field(init=False)
+    checkpoints: list[Checkpoint] = field(init=False, default_factory=list)
+    boost_used: bool = field(init=False, default=False)
+
+    def __post_init__(self):
+        self.state_machine = StateMachine(self.initial_state)
+        self.state_machine.set_context(self)
+
+    @property
+    def next_cp(self):
+        return self.checkpoints[0]
+
+
+class Game:
+    def __init__(self):
+        self.first_round = True
+        self.checkpoints = collections.deque()  # will be cycled later
+
+        # prepare player
+        self.player = Player(initial_state=FirstRound())
+        self.enemy = Pod()
+
+    @staticmethod
+    def read_state():
+        (
+            x, y,
+            next_checkpoint_x, next_checkpoint_y,
+            next_checkpoint_dist, next_checkpoint_angle
+        ) = map(int, input().split())
+        opponent_x, opponent_y = map(int, input().split())
+
+        player_pos = np.array([x, y])
+        enemy_pos = np.array([opponent_x, opponent_y])
+        next_cp_pos = np.array([next_checkpoint_x, next_checkpoint_y])
+        next_checkpoint = Checkpoint(
+            pos=next_cp_pos,
+            angle=next_checkpoint_angle,
+            distance=next_checkpoint_dist,
+        )
+
+        return player_pos, enemy_pos, next_checkpoint
+
+    def _add_checkpoint(self, checkpoint):
+        self.checkpoints.insert(1, checkpoint)
+        self.checkpoints.rotate(-1)
+
+    def update_state(self):
+        player_pos, enemy_pos, next_checkpoint = self.read_state()
+
+        # enemy
+        self.enemy.pos = enemy_pos
+
+        # checkpoints
+        self._add_checkpoint(next_checkpoint)
+
+        # player
+        self.player.pos = player_pos
+        self.player.checkpoints = self.checkpoints
+
+    def play(self):
+        while True:
+            self.update_state()
+            self.player.state_machine.action()
+
+
+class MockInput:
+    def __init__(self, responses):
+        self.responses = iter(responses)
+
+    def __call__(self, prompt=''):
+        return next(self.responses)
+
+
+# input = MockInput(['5089 4758 11505 6078 6550 0', '4963 5750'])
+
+g = Game()
+g.play()
